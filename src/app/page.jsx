9a2cb88f2c29be595d192edfbc2074dc63660654
omersenem <omerrsenem@gmail.com>
2023-09-28 14:47:49 +0300

    fix: tailwind eklendi.

---
+++ src/app/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const metadata = {
+    title: 'My Page Title',
+}
+
+const Page = () => {
+
+    return (
+        <h1 className="text-3xl font-bold ">
+            Hello world!
+        </h1>
+
+    )
+}
+export default Page
